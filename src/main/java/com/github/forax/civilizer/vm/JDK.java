d6b02e174b5a38be569b4f2ca17f5adb190ffc5c
forax <forax@univ-mlv.fr>
2023-02-16 11:30:09 +0100

    the class JDK is only used by reflection

--- src/main/java/com/github/forax/civilizer/vm/JDK.java
+++ src/main/java/com/github/forax/civilizer/vm/JDK.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.IntStream;
 
+@SuppressWarnings("unused")  // used by reflection
 public final class JDK {
   private JDK() {
     throw new AssertionError();
