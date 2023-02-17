d9d20176508bb2a565e52a1009a0750ff6fac80b
forax <forax@univ-mlv.fr>
2023-02-17 09:12:09 +0100

    split erase/identity into two methods to make the code more readable

--- src/main/java/com/github/forax/civilizer/vm/JDK.java
+++ src/main/java/com/github/forax/civilizer/vm/JDK.java
@@ -1,6 +1,7 @@
 package com.github.forax.civilizer.vm;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.IntStream;
 
@@ -16,22 +17,29 @@ public final class JDK {
   private static final ConcurrentHashMap<Location.Key, Object> CACHE = new ConcurrentHashMap<>();
 
   @SuppressWarnings("unused")  // used by reflection
-  public static Object erase(/*Location*/Object locationObj, /*List<?>*/Object parametersObj, /*List<?>*/Object defaultsObj) {
-    //System.out.println("erase " + locationObj + " " + parametersObj + " " + defaultsObj);
+  public static Object erase(Object locationObj, Object parametersObj, Object defaultsObj) {
     var location = (Location) locationObj;
     var parameterList = (List<?>) parametersObj;
     var defaultList = (List<?>) defaultsObj;
+    return erase(location, parameterList, defaultList);
+  }
+
+  @SuppressWarnings("WeakerAccess")  // used by reflection
+  public static Object erase(Location location, List<?> parameters, List<?> defaults) {
+    //System.out.println("erase " + location + " " + parameters + " " + defaults);
+    Objects.requireIdentity(location, "location is null");
+    Objects.requireIdentity(defaults, "defaults is null");
     List<?> erasedList;
-    if (parameterList == null) {
-      erasedList = defaultList;
+    if (parameters == null) {
+      erasedList = defaults;
     } else {
-      if (parameterList.size() != defaultList.size()) {
-        throw new LinkageError("instantiation arguments " + parameterList + " and default arguments " + defaultList + " have no the same size");
+      if (parameters.size() != defaults.size()) {
+        throw new LinkageError("instantiation arguments " + parameters + " and default arguments " + defaults + " have no the same size");
       }
-      erasedList = IntStream.range(0, parameterList.size())
+      erasedList = IntStream.range(0, parameters.size())
           .mapToObj(i -> {
-            var parameter = parameterList.get(i);
-            return parameter instanceof Class<?> clazz && com.github.forax.civilizer.runtime.RT.isSecondaryType(clazz) ? clazz : defaultList.get(i);
+            var parameter = parameters.get(i);
+            return parameter instanceof Class<?> clazz && com.github.forax.civilizer.runtime.RT.isSecondaryType(clazz) ? clazz : defaults.get(i);
           }).toList();
     }
     var key = location.key(erasedList);
@@ -39,8 +47,15 @@ public final class JDK {
   }
 
   @SuppressWarnings("unused")  // used by reflection
-  public static Object identity(/*Location*/Object locationObj, Object parameters) {
+  public static Object identity(Object locationObj, Object parametersObj) {
     var location = (Location) locationObj;
+    return identity(location, parametersObj);
+  }
+
+  @SuppressWarnings("WeakerAccess")  // used by reflection
+  public static Object identity(Location location, Object parameters) {
+    Objects.requireIdentity(location, "location is null");
+    Objects.requireIdentity(parameters, "parameters is null");
     var key = location.key(parameters);
     return CACHE.computeIfAbsent(key, k -> location.specialize(parameters));
   }
