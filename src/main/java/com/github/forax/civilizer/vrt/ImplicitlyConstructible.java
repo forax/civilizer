c455ea11cc990562b30d3b0d79d8a0b91e098219
forax <forax@univ-mlv.fr>
2024-08-18 12:40:30 +0200

    annotations should be runtime visible

--- src/main/java/com/github/forax/civilizer/vrt/ImplicitlyConstructible.java
+++ src/main/java/com/github/forax/civilizer/vrt/ImplicitlyConstructible.java
@@ -1,11 +1,12 @@
 package com.github.forax.civilizer.vrt;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.CLASS)
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target(TYPE)
+@Retention(RUNTIME)
 public @interface ImplicitlyConstructible {
 }
